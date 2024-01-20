8669d735c7538a0fdd20bd6965e7e860be0791e7
mambo <omiyer76@gmail.com>
2024-01-20 15:29:02 -0600

    basic orbit simulation system
    this is the basic foundation of the project, having bodies with different masses attracting and influencing each others velocity. kinda hard to figure out the starting values for a stable configuration, but i did do it with a single planet and a sun. this stuff uses newtons law of gravitation (F = G(m1m2)/R2). my next step will be to add a visualisation of the orbits.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,57 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
-
+let bodies = [];
+let G = 2;
+let timeStep = 1;
+let sunMass = 100;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800, WEBGL);
+  
+  bodies.push(new Body(sunMass, createVector(0,0,0), createVector(0,0,0), color(253, 184, 19)));
+  bodies.push(new Body(20, createVector(400,0,0), createVector(0,3,0), color(0,0,255)));
+  bodies.push(new Body(5, createVector(200,0,0), createVector(0,3,0), color(0,255,0)));
 }
 
 function draw() {
-  background(220);
+  background(0,0,50);
+  orbitControl(10,10,2);
+
+  for (let body of bodies) 
+    body.applyGravity();
+  for (let body of bodies) 
+    body.display();
+  
+}
+
+class Body {
+  constructor(mass, pos, vel, col) {
+    this.mass = mass;
+    this.pos = pos;
+    this.vel = vel;
+    this.col = col;
+  }
+
+  applyGravity() {
+    if(this.mass===sunMass)return;
+    for (let body of bodies) {
+      if (body !== this) {
+        let sqrDist = p5.Vector.sub(body.pos, this.pos).magSq();
+        let forceDir = p5.Vector.sub(body.pos, this.pos).normalize();
+        let force = p5.Vector.mult(forceDir, (G * this.mass * body.mass) / sqrDist);
+        
+        this.vel.add(force);
+      }
+    }
+  }
+
+
+
+  display() {
+    this.pos.add(this.vel);
+    push();
+    if(this.mass!==sunMass)noStroke();
+    fill(255);
+    translate(this.pos.x, this.pos.y, this.pos.z);
+    fill(this.col);
+    sphere(this.mass);
+    pop();
+  }
 }
