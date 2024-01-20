d2b5180455bc6c5e2c1f6a2a408d00756eced654
mambo <omiyer76@gmail.com>
2024-01-20 17:02:24 -0600

    orbits
    it runs kind of slow with the orbits as its drawing from 10k points every frame for every planet(not sun), but i guess it works. it runs more smoothly with like 5k points instead, but the orbit is shorter. i guess this can be used to figure out the initial values for position and velocity of the bodies, and then running the program after youve found a good uniform orbit. the way this works is that in setup, for a specified number of steps (10k at default), it runs the simulation with all the bodies, and puts their position after each step into their own "orbit" array. after the 10k steps, their position and velocities are set back to whatever they were at the beginning. and then every frame in the display function of bodies, it also draws the orbit by drawing from all the 10k positions in the orbit array for the corresponding body.

--- sketch.js
+++ sketch.js
@@ -1,19 +1,35 @@
 let bodies = [];
-let G = 2;
-let timeStep = 1;
-let sunMass = 100;
+let G = 1;
+let sunMass = 5000;
+
+
 function setup() {
   createCanvas(800, 800, WEBGL);
   
-  bodies.push(new Body(sunMass, createVector(0,0,0), createVector(0,0,0), color(253, 184, 19)));
-  bodies.push(new Body(20, createVector(400,0,0), createVector(0,3,0), color(0,0,255)));
-  bodies.push(new Body(5, createVector(200,0,0), createVector(0,3,0), color(0,255,0)));
+  bodies.push(new Body(sunMass, createVector(0,0,0), createVector(0,0,0), color(253, 184, 19), 500));
+  bodies.push(new Body(100, createVector(2000,0,0), createVector(0,16,0), color(0,0,255)));
+  bodies.push(new Body(200, createVector(3500,0,0), createVector(0,17.5,0), color(0,255,0)));
+  camera(0,0,8000);
+
+  for(let i = 0; i<10000; i++){
+    for(let body of bodies)
+      body.applyGravity();
+    for(let body of bodies){
+      body.pos.add(body.vel);
+      if(body.mass!==sunMass)
+        body.orbit.push(body.pos.copy());
+    }
+  }
+  for(let body of bodies){//reset vars
+    body.pos=body.tpos.copy();
+    body.vel=body.tvel.copy();;
+  }
+  frameRate(24);
 }
 
 function draw() {
   background(0,0,50);
   orbitControl(10,10,2);
-
   for (let body of bodies) 
     body.applyGravity();
   for (let body of bodies) 
@@ -22,18 +38,20 @@ function draw() {
 }
 
 class Body {
-  constructor(mass, pos, vel, col) {
+  constructor(mass, pos, vel, col, size=mass) {
     this.mass = mass;
-    this.pos = pos;
-    this.vel = vel;
+    this.pos = pos; this.tpos=pos.copy();
+    this.vel = vel; this.tvel=vel.copy();
     this.col = col;
+    this.size=size;
+    this.orbit=[];
   }
 
   applyGravity() {
-    if(this.mass===sunMass)return;
+    if(this.mass===sunMass)return; // ignore the sun
     for (let body of bodies) {
       if (body !== this) {
-        let sqrDist = p5.Vector.sub(body.pos, this.pos).magSq();
+        let sqrDist = p5.Vector.sub(body.pos, this.pos).magSq(); //https://www.britannica.com/science/Newtons-law-of-gravitation
         let forceDir = p5.Vector.sub(body.pos, this.pos).normalize();
         let force = p5.Vector.mult(forceDir, (G * this.mass * body.mass) / sqrDist);
         
@@ -42,16 +60,21 @@ class Body {
     }
   }
 
-
-
   display() {
     this.pos.add(this.vel);
+    noFill();
+    stroke(this.col);
+    strokeWeight(5);
+    beginShape();
+    for (let p of this.orbit) 
+      vertex(p.x, p.y, p.z);
+    endShape();
     push();
     if(this.mass!==sunMass)noStroke();
-    fill(255);
+    else stroke(0);
     translate(this.pos.x, this.pos.y, this.pos.z);
     fill(this.col);
-    sphere(this.mass);
+    sphere(this.size);
     pop();
   }
 }
